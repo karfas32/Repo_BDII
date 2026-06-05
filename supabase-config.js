cc1724141fa8764f7189c82ef482f21e7e4c1b16
karfas32 <karfas_2@hotmail.com>
2026-06-05 18:48:37 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
 // Configuración de Supabase - UPLA Proyecto BD2
 const SUPABASE_URL = "https://ckmggqrangyssmphbxgw.supabase.co";
+
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrbWdncXJhbmd5c3NtcGhieGd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMDU0MTMsImV4cCI6MjA5MTc4MTQxM30.ol2HlfOzHAd6_Z90OMPIi5pbKrOnGtOeNsaOizM5lCI";
 
 // Inicializar el cliente de Supabase
