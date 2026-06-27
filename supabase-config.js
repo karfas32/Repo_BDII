dccc5e4078fce7ea7b3cf3d6d912c06245aee178
karfas32 <karfas_2@hotmail.com>
2026-06-27 08:24:29 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -8,6 +8,6 @@ const SUPABASE_URL = "https://ckmggqrangyssmphbxgw.supabase.co"; // URL Base cor
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrbWdncXJhbmd5c3NtcGhieGd3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NjIwNTQxMywiZXhwIjoyMDkxNzgxNDEzfQ.gvPdY8q7QB16NEx-LyKBvLpSGRfKfNBles4kX3pgEOs"; 
 
 // Inicializar el cliente global de Supabase
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 console.log("✅ Supabase configurado correctamente con la URL Base.");
