33fce1caaaea40831406887ef390196d4ce3d070
karfas32 <karfas_2@hotmail.com>
2026-06-26 21:02:56 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -5,7 +5,7 @@
 const SUPABASE_URL = "https://ckmggqrangyssmphbxgw.supabase.co"; // URL Base corregida (sin /rest/v1/)
 
 // Reemplaza esta cadena con tu clave pública "anon / public" real desde el panel de Supabase -> Project Settings -> API
-const SUPABASE_KEY = "sb_secret_sGYtTvq7pHkDnzr6Fjw06Q_BnJAd7K9"; 
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrbWdncXJhbmd5c3NtcGhieGd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMDU0MTMsImV4cCI6MjA5MTc4MTQxM30.ol2HlfOzHAd6_Z90OMPIi5pbKrOnGtOeNsaOizM5lCI"; 
 
 // Inicializar el cliente global de Supabase
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
